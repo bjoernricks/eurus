0544932844fe39e74a57011c79e29a5b69074642
Björn Ricks <bjoern.ricks@greenbone.net>
2023-12-06 11:18:03 +0100

    Add: Introduce a base error class

---
+++ eurus/errors.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+class EurusError(Exception):
+    """
+    Base class for all errors originating in eurus
+    """
